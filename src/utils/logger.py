7c3dd0adb2001466ea703eda99fb28c80ae533c3
KaholiK <131408085+KaholiK@users.noreply.github.com>
2024-12-09 19:22:20 -0500

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,1 +1,9 @@
+# src/utils/logger.py
 
+from loguru import logger
+import sys
+
+def setup_logger():
+    logger.remove()  # Remove default logger
+    logger.add(sys.stdout, level="INFO")
+    logger.add("logs/quantumtrader.log", rotation="1 week", retention="4 weeks", compression="zip")
